a25460dfadcf552ebb79214c1b9ba44897dc7bbd
Braeden <113859768+BraedenC2@users.noreply.github.com>
2023-09-19 10:31:51 -0500

    Add files via upload

---
+++ JavaScript/Lesson1/index.js
@@ -0,0 +1,9 @@
+// 3 different declaration types of variables are: (var, let, const)
+
+let age = 21;
+let firstName = "Braeden";
+let student = true;
+
+console.log(age);
+
+document.getElementById("p1").innerHTML = "Hello, " + firstName + ".";
---
+++ JavaScript/Lesson3/index.js
@@ -0,0 +1,15 @@
+let age = window.prompt("How old are you?");
+let amount = 10;
+let boo = "pizza";
+
+// Converts a String to a number
+age = Number(age);
+
+// Converts a number to String
+amount = String(10);
+
+boo = Boolean("pizza");
+
+age += 1;
+
+console.log("You are now ", age, " years old.");
